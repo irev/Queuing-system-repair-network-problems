6098009961f7669992fed31c52c168652ab72265
Andra <refyandra@gmail.com>
2016-08-12 00:48:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ if(isset($_SESSION['login_hash'])){
           <p>- OR -</p>
           <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
           <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
-        </div><!-- /.social-auth-links -->
+        </div><--.social-auth-links -->
 <!--div class="box-footer">
         <a href="#">I forgot my password</a><br>
         <a href="register.html" class="text-center">Register a new membership</a>
